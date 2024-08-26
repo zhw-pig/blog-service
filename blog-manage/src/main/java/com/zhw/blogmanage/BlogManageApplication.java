65639233e50a7dfebff82797284ddddff25f4267
张怀伟 <zhanghuaiwei@southzn.com>
2024-08-26 14:49:52 +0800

    项目初始化

---
+++ blog-manage/src/main/java/com/zhw/blogmanage/BlogManageApplication.java
@@ -0,0 +1,13 @@
+package com.zhw.blogmanage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogManageApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BlogManageApplication.class, args);
+    }
+
+}
---
+++ blog-manage/src/test/java/com/zhw/blogmanage/BlogManageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zhw.blogmanage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogManageApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
